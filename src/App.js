a8f01e8d748351db00974fe71af6714b8a7f5bba
Salehjarad <salehjarad.art@gmail.com>
2020-07-19 16:53:11 +0300

    Editing main page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo2.svg';
+import logo2 from './logo_imdad2.svg';
+import logo3 from './uc.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <img src={logo2} className='App-logo-2' alt='Emdad' />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" width={300}/>
+        <img src={logo3} alt="logo" width={300}/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          الموقع تحت الصيانة
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <footer>
+        <p>Copyright © 2020 Emdad Co. All rights reserved</p>
+      </footer>
     </div>
   );
 }
